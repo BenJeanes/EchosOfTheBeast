b5d14418625740d4ac57ff5fd382c86f0391bec1
Jayson Miller <unevenranger@live.co.uk>
2017-10-28 22:41:11 +0100

    - Added Navmesh to the Test Level - Created cube to represent minotaur - Added code to Minotaur to make it move from it's current position to another preset position

---
+++ Echos of the beast/Assets/Scripts/MinotaurNav.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MinotaurNav : MonoBehaviour 
+{
+	//public variables
+	[SerializeField]
+	Transform destination;
+
+	private NavMeshAgent navMeshAgent;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+		if (navMeshAgent == null) {
+			Debug.LogError ("The NavMeshAgent is not attached to " + gameObject.name);
+		} 
+		else 
+		{
+			SetDestination ();
+		}
+	}
+	
+	private void SetDestination()
+	{
+		if (destination != null) 
+		{
+			Vector3 targetVector = destination.transform.position;
+			navMeshAgent.SetDestination (targetVector);
+		}
+	}
+}
