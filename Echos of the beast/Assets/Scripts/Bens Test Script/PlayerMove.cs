6bfe16d1b5b2984754b3e5992d7f1ed30a6e8955
Unknown <benjamin.jeanes@students.plymouth.ac.uk>
2017-10-30 14:10:19 +0000

    Movement prototype working in movetest scene

--- Echos of the beast/Assets/Scripts/Bens Test Script/PlayerMove.cs
+++ Echos of the beast/Assets/Scripts/Bens Test Script/PlayerMove.cs
@@ -6,58 +6,36 @@ public class PlayerMove : MonoBehaviour
 {
     public GameObject anchor;
     private GameObject player;
-    public GameObject progGo;
     public Camera c;
-    bool moving;
-    public float scalar;
-    private float prog;
-    
+    bool moving;    
 
     void Awake()
     {
         moving = false;
-        player = this.gameObject;
-        scalar = 2f;
+        player = this.gameObject;        
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            anchor.gameObject.SetActive(true);
-            anchor.transform.position = c.ScreenToWorldPoint(Input.mousePosition);
-            ClampY(anchor);
+            anchor.gameObject.SetActive(true);            
             moving = true;
-            prog = 0;
         }
         if(Input.GetMouseButtonUp(0))
         {
             moving = false;            
-            anchor.gameObject.SetActive(false);
-            ClampY(anchor);
+            anchor.gameObject.SetActive(false);            
         }
         if(moving)
         {
-            Vector3 dir = new Vector3(c.ScreenToWorldPoint(Input.mousePosition).x, 0.5f, c.ScreenToWorldPoint(Input.mousePosition).z);
-            Vector3 distToMove = anchor.transform.position - player.transform.position;
-            Debug.DrawLine(anchor.transform.position, player.transform.position, Color.blue);
-            Debug.DrawRay(player.transform.position, anchor.transform.position - dir, Color.red);
-            
-            Debug.Log(prog);
-            progGo.transform.position = distToMove * prog;
-
-            //player.GetComponent<Rigidbody>().AddForce((anchor.transform.position - player.transform.position) * scalar);                  
+            anchor.transform.position = ClampY(c.ScreenToWorldPoint(Input.mousePosition));                      
+            player.transform.position = Vector3.MoveTowards(player.transform.position, anchor.transform.position, 0.1f);                      
         }
     }
-
-    private void ClampY(GameObject o)
+    private Vector3 ClampY(Vector3 v)
     {
-        o.transform.position = new Vector3(o.transform.position.x, 0.5f, o.transform.position.z);
+        v = new Vector3(v.x, 0.5f, v.z);
+        return v;
     }
-
-    private void Lerp(Vector3 start, Vector3 end)
-    {
-
-    }
-
 }
