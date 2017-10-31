f93cb4b7a62366ec66997042a6bec816ff24e9ff
Unknown <benjamin.jeanes@students.plymouth.ac.uk>
2017-10-31 15:33:19 +0000

    Basic player movement

--- Echos of the beast/Assets/Scripts/ControllerPlaceAnchor.cs
+++ Echos of the beast/Assets/Scripts/ControllerPlaceAnchor.cs
@@ -6,41 +6,58 @@ public class ControllerPlaceAnchor : MonoBehaviour
 {
     private SteamVR_TrackedObject trackedObj;
     public GameObject anchor;
-    public GameObject player;
-    public GameObject VR_PlayArea;
+
+    public GameObject VR_Camera;
+    public GameObject VR_Rig;
+
+    public float scalar;
+
     private SteamVR_Controller.Device Controller
     {
         get { return SteamVR_Controller.Input((int)trackedObj.index); }
     }
     bool moving = false;
-    Vector3 startpos;
-    float speed = 5.0f;
-    
+
+    Vector3 controllerPosAtTrigDown;
+    Vector3 camPosAtTrigDown;
+    Vector3 rigPosAtTrigDown;
+
+    Vector3 change;    
 
     private void Awake()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();       
-    }
-
-    // Update is called once per frame
-    void Update ()
+    }    
+    private void Update()
     {
-        Debug.DrawRay(player.transform.position, (anchor.transform.position - player.transform.position) * 10, Color.red);     
-        if (Controller.GetHairTriggerDown())
-        {            
-            anchor.transform.position = this.transform.position;
-            moving = true;
-            startpos = VR_PlayArea.transform.position;
-        }
-        if (Controller.GetHairTriggerUp())
+        if(Controller.GetHairTriggerDown())
         {
-            float step = speed * Time.deltaTime;
-            VR_PlayArea.transform.position = Vector3.MoveTowards(startpos, anchor.transform.position - player.transform.position, step);
+            moving = true;
+            controllerPosAtTrigDown = this.transform.position;
+            camPosAtTrigDown = VR_Camera.transform.position;
+            rigPosAtTrigDown = VR_Rig.transform.position;
+        }
+        if(Controller.GetHairTriggerUp())
+        {
+            moving = false;
+            //Debug.Log(string.Format("{0} * {1} * (1 + {2})", change, scalar, this.GetComponent<Rigidbody>().velocity.magnitude));
         }
         if(moving)
-        {          
+        {
             
+            change = controllerPosAtTrigDown - this.transform.position;
+            change = ClampYToZero(change);
+            VR_Rig.GetComponent<Rigidbody>().AddForce(change * scalar * (1 + this.GetComponent<Rigidbody>().velocity.magnitude));
+            if(VR_Rig.GetComponent<Rigidbody>().velocity.magnitude > 0.5f)
+            {
+                //Debug.Log(VR_Rig.GetComponent<Rigidbody>().velocity.magnitude);
+            }
         }
+        Debug.Log(this.GetComponent<Rigidbody>().velocity.magnitude);
+    }
 
+    private Vector3 ClampYToZero(Vector3 v)
+    {
+        return v = new Vector3(v.x, 0, v.z);
     }
 }
