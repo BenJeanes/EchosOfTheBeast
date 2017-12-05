df526095babc7c66f0f26930edfd669254dccae6
Unknown <benjamin.jeanes@students.plymouth.ac.uk>
2017-12-05 17:08:28 +0000

    Started work on emergency non vr fp controller

---
+++ Echos of the beast/Assets/FPController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 60.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        Debug.DrawLine(transform.position, transform.forward, Color.blue);
+	}
+}
