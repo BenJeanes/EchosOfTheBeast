4bbc234e0afa82943f73845bc5553ccc45e62005
Sam Davey <samkdavey@gmail.com>
2017-12-05 20:31:37 +0000

    Win and Lose System Complete

---
+++ Echos of the beast/Assets/GameStateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Minotaur")
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
+        else if (collision.gameObject.tag == "Stairs")
+        {
+            SceneManager.LoadScene("WinScreen");
+        }
+    }
+}
