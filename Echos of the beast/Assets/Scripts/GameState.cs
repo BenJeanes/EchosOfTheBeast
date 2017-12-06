e7a0be00d07c3e04d5d07ac6dfd6a3bc557da99c
Sam Davey <samkdavey@gmail.com>
2017-12-06 12:03:07 +0000

    Rush commint from after demo

---
+++ Echos of the beast/Assets/Scripts/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+       
+
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Minotaur")
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
+        else if (collision.gameObject.tag == "Stairs")
+        {
+            SceneManager.LoadScene("WinScreen");
+        }
+    }
+}
